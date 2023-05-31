c90df92cb07e58dff0339581f8c7014b75f3e4e3
Abdul Rahman Vakili <vakili@univention.de>
2023-05-31 13:26:01 +0200

    Added-text file

--- GUI-Proj/Todo-GUI/Todo-list-gui.py
+++ GUI-Proj/Todo-GUI/Todo-list-gui.py
@@ -6,15 +6,26 @@ sudo apt-get install python3.10-tk
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 #del_button = sg.Button("Delete")
 #window = sg.Window('My To-Do App', layout=[[label, input_box]])  # both label and input_txt in save row!
 #window = sg.Window('My To-Do App', layout=[[label], [input_box], [add_button], [del_button]]) # In different rows!, here based on list, 4 rows!
-window = sg.Window('My To-Do App', layout=[[label, input_box, add_button]]) # In two rows!
+window = sg.Window('My To-Do App',           # In two rows!
+                   layout=[[label, input_box, add_button]],
+                   font=('Helvetica', 14))
 
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
 
-window.read()
 print("Test!")
 window.close()
 
--- GUI-Proj/Todo-GUI/functions.py
+++ GUI-Proj/Todo-GUI/functions.py
@@ -4,13 +4,11 @@ def get_todos(filename=FILEPATH):
     with open(filename, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
-
 #Note: if we use a default parameter, then non-default parameters should be placed before it!
 #def write_todos(filepath="save-todo.txt", todos_arg):
-def write_todos(todos_arg, filepath="save-todo.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
-
 
 #Some time we need some code (functions.py) to be "executed" when it is executed "directly" but not when executed via another code/file like "main.py"
 #print(get_todos())
