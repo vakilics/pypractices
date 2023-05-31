cdbfb2c7e6d21b2d1e8d21e0fc3c72d8b0ac5d53
Abdul Rahman Vakili <vakili@univention.de>
2023-05-31 15:29:08 +0200

    feat: Edit button

--- GUI-Proj/Todo-GUI/Todo-list-gui.py
+++ GUI-Proj/Todo-GUI/Todo-list-gui.py
@@ -8,25 +8,47 @@ import PySimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("Edit")
+
 #del_button = sg.Button("Delete")
 #window = sg.Window('My To-Do App', layout=[[label, input_box]])  # both label and input_txt in save row!
 #window = sg.Window('My To-Do App', layout=[[label], [input_box], [add_button], [del_button]]) # In different rows!, here based on list, 4 rows!
-window = sg.Window('My To-Do App',           # In two rows!
-                   layout=[[label, input_box, add_button]],
+window = sg.Window('My To-Do App',           # In two rows, ...!
+                   layout=[[label, input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 14))
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
+
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)  #to show the todolist after Add in the window
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':    #to show the text we select to edit, in Input box!
+            window['todo'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:   # if we click on close icon, then will no more error happens!
             break
+
 
 
 print("Test!")
