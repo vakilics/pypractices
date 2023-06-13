3ad9c3c9be0ac47705855bdad4eab29a95cc0a93
Abdul Rahman Vakili <vakili@univention.de>
2023-06-13 17:54:18 +0200

    Completed Zip creator app

--- GUI-Proj/Zipper-GUI/zipcreator.py
+++ GUI-Proj/Zipper-GUI/zipcreator.py
@@ -5,8 +5,8 @@ def make_archive(filepaths, dest_dir):
     dest_path = pathlib.Path(dest_dir, "mycompressed.zip")   #makes the path: dest/compressed.zip
     with zipfile.ZipFile(dest_path, 'w') as archive:
         for filepath in filepaths:
-            archive.write(filepath)
-
+            filepath = pathlib.Path(filepath)   #to get the filename itself not full path
+            archive.write(filepath, arcname=filepath.name)
 
 if __name__ == "__main__":
     make_archive(filepaths=["file1zip.txt", "file2zip.txt"], dest_dir="dest")
--- GUI-Proj/Zipper-GUI/zipper.py
+++ GUI-Proj/Zipper-GUI/zipper.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
-label1 = sg.Text("Select files to compress:")
+from zipcreator import make_archive
+
+label1 = sg.Text("Select file")
 input1 = sg.Input()
 choose_button1 = sg.FileBrowse("Choose", key="files")
 
@@ -8,17 +10,22 @@ input2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose", key="folder")
 
 compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output", text_color="green")
 
 window = sg.Window("File Compressor",
                    layout=[[label1, input1, choose_button1],
                            [label2, input2, choose_button2],
-                           [compress_button]])
+                           [compress_button, output_label]])
+
+
 
 while True:
     event, values = window.read()
     print(event,values)
-    filepath = values["Choose"].split(";")
+    filepaths = values["files"].split(";")
     folder = values["folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(background_color="yellow",  value="Compresseion Completed!")  #will print this text next to the "Compress" cutton in out GUI app!
 
 
 window.read()
