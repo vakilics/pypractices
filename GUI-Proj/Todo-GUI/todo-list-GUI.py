f7e4a240ed7530f9a68a680dcc55bbe4dbafa56f
Abdul Rahman Vakili <vakili@univention.de>
2023-06-14 13:14:17 +0200

    Use images as button

--- GUI-Proj/Todo-GUI/todo-list-GUI.py
+++ GUI-Proj/Todo-GUI/todo-list-GUI.py
@@ -14,7 +14,11 @@ sg.theme("Black")
 clock = sg.Text('', key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add", size=5)
+
+#Can add simple button or make it more visible!
+#add_button = sg.Button("Add", size=5)
+add_button = sg.Button(size=5, image_source="GUI-Proj/Todo-GUI/add.png", mouseover_colors="LightBlue2", tooltip="Add Todo", key="Add")
+
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])  # 45 character, number of rows-high
 edit_button = sg.Button("Edit")
