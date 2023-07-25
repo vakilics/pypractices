103e50464c59a2d08603db245d26cf61c9ef3b91
Abdul Rahman Vakili <vakili@univention.de>
2023-07-25 18:28:34 +0200

    initial web App ...!

--- GUI-Proj/Todo-GUI/web.py
+++ GUI-Proj/Todo-GUI/web.py
@@ -1,14 +1,25 @@
 #easty to integrate with web apps using "stramlit"
+# By refreshing the web page, python code will be executed from top to button!
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
 
-st.title("Welcome Mr. Abdul Rahman Vakili")
+def add_todo():
+    todo = st.session_state["my_new_todo"] + "\n"
+    #print(todo)
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("Welcome Vakili's To-Do Web App!")
 st.subheader("This is my todo app!")
 st.write("This App helps manage your tasks! ")
 
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Add To-DO!", placeholder="something...")
+st.text_input(label="Add To-DO!", placeholder="something...", on_change=add_todo, key='my_new_todo')
+
+#print("End of App!")
+st.session_state
