7fc16f93b37f6b58eb140f28119777360c257ec3
Abdul Rahman Vakili <vakili@univention.de>
2023-07-25 17:55:31 +0200

    initial web App!

--- GUI-Proj/Todo-GUI/web.py
+++ GUI-Proj/Todo-GUI/web.py
@@ -1,3 +1,14 @@
 #easty to integrate with web apps using "stramlit"
 import streamlit as st
+import functions
 
+todos = functions.get_todos()
+
+st.title("Welcome Mr. Abdul Rahman Vakili")
+st.subheader("This is my todo app!")
+st.write("This App helps manage your tasks! ")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="Add To-DO!", placeholder="something...")
