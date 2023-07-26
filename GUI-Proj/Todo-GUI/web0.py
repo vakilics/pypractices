92ec927d608d81f6ddc1c9002fb6328176fb016f
Abdul Rahman Vakili <vakili@univention.de>
2023-07-26 10:13:46 +0200

    initial web app

--- GUI-Proj/Todo-GUI/web0.py
+++ GUI-Proj/Todo-GUI/web0.py
@@ -3,7 +3,9 @@
 import streamlit as st
 import functions
 
+
 todos = functions.get_todos()
+
 def add_todo():
     todo = st.session_state["my_new_todo"] + "\n"
     #print(todo)
@@ -14,15 +16,8 @@ st.title("Welcome Vakili's To-Do Web App!")
 st.subheader("This is my todo app!")
 st.write("This App helps manage your tasks! ")
 
-for index, todo in enumerate(todos):
-    #st.checkbox(todo, key=todo) #will show each checkbox status
-    checkbox = st.checkbox(todo, key=todo)
-    if checkbox:   #means: if checkbox is checked/ticked
-        #print(checkbox)
-        todos.pop(index)
-        functions.write_todos(todos) #again write the updated todos without the removed todo.
-        del st.session_state[todo]
-        st.experimental_rerun()
+for todo in todos:
+    st.checkbox(todo)
 
 st.text_input(label="Add To-DO!", placeholder="something...", on_change=add_todo, key='my_new_todo')
 
